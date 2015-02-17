386c2eb58d15d9e75ff0a5ad3acbd91754ae6eb8
Douglas G. Scofield <douglasgscofield@gmail.com>
2015-02-17 07:38:18 +0100

    first work on command indel-apply

--- TODO.md
+++ TODO.md
@@ -4,3 +4,9 @@ TODO
 * Convert pileVar.pl to pacbio-util
 * It makes more sense to call samtools from within the script to verify that we are generating the pileup that we want, and to make the interface easier for users to manage.
 * Come up with some options and common-sense defaults for when to generate targets
+* Use Bio::DB::Sam?
+* Should I include the pieces of BioPerl I use?
+* In Perl, how do I get a list of the exact modules I use, to help with the above?
+* Combine indel-targets and indel-apply?
+* We use BioPerl
+* Consider embedding assembly filename in the targets file as the first line, behind #
--- pacbio-util.pl
+++ pacbio-util.pl
@@ -2,14 +2,14 @@
 
 use strict;
 use warnings;
+# base modules
 use POSIX;
 use File::Which;
 use Getopt::Long;
 use List::Util qw/sum/;
-
-my $o_fasta;
-my $o_BAM;
-my $o_stdin;
+# BioPerl modules
+use Bio::Seq;
+use Bio::SeqIO;
 
 my $o_mappingquality = 0;
 my $o_mincov = 0;
@@ -28,7 +28,7 @@ my $o_indeltoobig = 1000;
 sub print_usage_and_exit($$);
 sub merge_pileup_columns($);
 sub indel_targets();
-sub target_apply();
+sub indel_apply();
 
 ################
 # main loop
@@ -37,8 +37,8 @@ my $command = shift @ARGV;
 
 if ($command eq "indel-targets") {
     indel_targets();
-} elsif ($command eq "target-apply") {
-    target_apply();
+} elsif ($command eq "indel-apply") {
+    indel_apply();
 }
 
 ################
@@ -92,6 +92,9 @@ pacbio-util.pl indel-targets -f FASTA FILE1.bam [ FILE2.bam ... ]
 
 ";
 
+    my $o_fasta;
+    my $o_stdin;
+
     GetOptions(""          => \$o_stdin,
                "fasta=s"   => \$o_fasta
     ) or print_usage_and_exit($usage, "unknown option");
@@ -101,7 +104,9 @@ pacbio-util.pl indel-targets -f FASTA FILE1.bam [ FILE2.bam ... ]
     my $samtools = which('samtools') or die "$command: samtools not found";
 
     my $samtools_pipe = "$samtools mpileup -s -BQ0 -d 1000000 -L 1000000 -f $o_fasta ".join(" ", @BAM)." |";
-    open PILEUP, $samtools_pipe or die "Could not initiate samtools pipe: $!";
+    open(PILEUP, $samtools_pipe) or die "Could not initiate samtools pipe: $!";
+
+    print STDOUT "#assembly:$o_fasta\n";
 
     while (<PILEUP>) {
         next if m/^#/;
@@ -162,7 +167,82 @@ pacbio-util.pl indel-targets -f FASTA FILE1.bam [ FILE2.bam ... ]
 
 ################
 
-sub target_apply() {
-    print STDERR "you've reached target_apply\n";
+sub find_ref_fasta_seq($);
+
+my $ref_fasta_io;   # SeqIO object for reading assembly
+my $ref_fasta_seq;  # current Seq object from assembly
+
+
+sub indel_apply() {
+    print STDERR "you've reached indel_apply\n";
+    my $usage = "
+pacbio-util.pl indel-apply -f FASTA -t TARGETS
+
+Correct indels within FASTA by applying TARGETS.  The order of sequences in FASTA
+and targets in TARGETS must be the same.
+
+    -f | --fasta FILE        PacBio assembly in Fasta format, to be corrected.
+                             Must be the same assembly used for
+                             '@0 indel-targets -f FILE ...'
+    -t | --targets TARGETS   List of indel targets to apply, output from
+                             '@0 indel-targets -f FILE ...'
+
+    --help, -?               help message
+
+";
+
+    # options for 
+
+    my $o_fasta;
+    my $o_targets;
+    my $o_stdin;
+
+    GetOptions(""          => \$o_stdin,
+               "fasta=s"   => \$o_fasta,
+               "targets=s" => \$o_targets
+    ) or print_usage_and_exit($usage, "unknown option");
+
+    open(TARGETS, "<$o_targets") or die "Could not open targets file '$o_targets': $!";
+    my $target_assembly = <TARGETS>;  # first line is '#assembly:filename'
+    die "First line of '$o_targets' does not contain assembly filename" if $target_ref !~ m/^#assembly:/;
+    chomp $target_assembly;
+    $target_assembly = substr($target_assembly, 10);  # remove #assembly:
+    if (not defined $o_fasta) {
+        print STDERR "Opening target assembly '$target_assembly'\n" if not $o_fasta;
+        $o_fasta = $target_assembly;
+    } elsif ($o_fasta ne $target_assembly) {
+        print STDERR "\n*** Warning: -f and target assembly do not match\n\n";
+    }
+    $ref_fasta_io = Bio::SeqIO->new(-file => "<$o_fasta", -format => "fasta") or 
+                    die "Could not open fasta file '$o_fasta': $!";
+
+    while (<TARGETS>) {
+        next if m/^#/;
+        chomp;
+        # ?what is the target line format?
+        # skip 'bad' indels
+        # if the reference sequence is new, write out the previous one
+        # get the indel to apply (generally a single-base insertion, yes?)
+        # apply it to the reference sequence
+    }
+    # write out the reference sequence
+}
+
+sub find_ref_fasta_seq($) {
+    # read fasta sequences until we load the requested one
+    my $seq_name_to_find = shift;
+    # if we return below, then we are already at the sequence of interest
+    if (defined($ref_fasta_seq) and $ref_fasta_seq->id eq $seq_name_to_find) {
+        #print STDERR "find_ref_fasta_seq: we are already at the reference sequence $seq_name_to_find\n";
+        return;
+    }
+    while (my $s = $ref_fasta_io->next_seq()) {
+        if ($s->id eq $seq_name_to_find) {
+            $ref_fasta_seq = $s;
+            #print STDERR "find_ref_fasta_seq: found new reference sequence $seq_name_to_find\n";
+            return;
+        }
+    }
+    die("couldn't find reference $seq_name_to_find");
 }
 
